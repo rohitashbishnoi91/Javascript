50048fa477ec0dc63ea271f9ad8f5f191f183a57
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-06-05 05:41:01 +0000

    first change done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("Rohitash")
+const accountId=B21BB026
+let accountEmail="rohitashbishnoi852@gmail.com"
+var accountPassword="1234"
+accountCity="Jodhpur"
+let accountState;
+
+
+//accountId cannot be changed
+
+accountEmail="bishnoi.3@iitj.ac.in"
+accoutPassword="3214"
+accountCity="Jodhpur"
+console.log(accountId)
+
+/*prefer not to use var keyword while declaring a variable
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

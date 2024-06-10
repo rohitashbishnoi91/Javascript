bd9cf24d30dc43f9a2c9b4f54b394e1bd9216932
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-06-10 16:49:37 +0000

    another one

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //use all JS code as new version
+
+//alert("hello") we are using nodejs not browser
+
+console.log(3+
+    3) //code readability should be high
+
+let name="Rohitash"
+let isLoggedIn=true
+let age=21
+let state=null
+
+//number-> 2 to the power 53
+//string 
+//bigint
+//null->standalone value
+//undefined
+//symbol-> uniqueness of a component
+
+//object
+
+console.log(typeof"rohit")
+
+
+//datatype conversion
+let a="33"
+console.log(typeof a)
+let c="33abc"
+console.log(typeof c)
+console.log(c)
+
+//"33"->number,33
+//"33abc"->number,NaN
+//"null"

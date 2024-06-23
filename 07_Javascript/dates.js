51f4177d2ed59c27414e45c4ed3eea198c285839
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-06-23 07:34:34 +0000

    date done

---
+++ 07_Javascript/dates.js
@@ -0,0 +1,13 @@
+let myDate=new Date();
+//console.log(myDate);
+//console.log(myDate.toDateString);
+//console.log(myDate.toISOString);
+//console.log(myDate.toJSON);
+//console.log(myDate.toString);
+/*let a=new Date(2003,3,11)
+console.log(a.toDateString())*/
+
+let newDate=new Date()
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getDay())

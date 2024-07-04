9166e50a0374de12f74d6fb1843552b6c7a9dbad
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-07-04 12:22:55 +0530

    Update one.js

--- Iterations/one.js
+++ Iterations/one.js
@@ -1,27 +1,27 @@
-/*for(let index=0;index<10;index++){
+for(let index=0;index<10;index++){
     const element=index
     if(element==5){
         console.log("5 is the best number")
     }
     console.log(index)
-}*/
+}
 
-/*for(let i=0;i<11;i++){
+for(let i=0;i<11;i++){
     console.log(`outer loop value is : ${i}`)
     for(let j=0;j<11;j++){
         console.log(`inner loop value is : ${j}`)
     }
-}*/
+}
 
 let myArray=["flash","batman","superman"]
 
 for(let index=0;index<myArray.length;index++){
     const element=myArray[index]
-    //console.log(element)
+    console.log(element)
 }
 
 //keywords
-//break an continue
+//break and continue
 
 for(let i=1;i<=20;i++){
     if(i==5) continue

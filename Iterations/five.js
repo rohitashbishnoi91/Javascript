1124f81c82115f1288adf5c39e7a2f95bd728310
rohitashbishnoi91 <123815174+rohitashbishnoi91@users.noreply.github.com>
2024-07-04 12:24:15 +0530

    Update five.js

--- Iterations/five.js
+++ Iterations/five.js
@@ -1,13 +1,13 @@
 const coding=["java","python","javascript","cpp"]
 
 coding.forEach( function(item){ //its a callback function so it doesnt need to be named
-   // console.log(item)
+    console.log(item)
 })
 
 //arrow function revision
-// name = (parameter) => {}
+name = (parameter) => {}
 
-    //coding.forEach( (item) => {console.log(item)} )
+coding.forEach( (item) => {console.log(item)} )
 
 function Print(item){
 console.log(item)
